94e2420ecbb6e2cba94fc34bc33ff1b321288252
Owen Mecham <owen.mecham@adesa.com>
2021-03-17 21:50:58 -0600

    feat: added material design

--- projects/mfe1/src/app/app.module.ts
+++ projects/mfe1/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { APP_ROUTES } from './app.routes';
 import { CryptosModule } from './cryptos/cryptos.module';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, CryptosModule, RouterModule.forRoot(APP_ROUTES)],
+  imports: [BrowserModule, CryptosModule, RouterModule.forRoot(APP_ROUTES), BrowserAnimationsModule],
   declarations: [HomeComponent, AppComponent],
   providers: [],
   bootstrap: [AppComponent],
--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewChild, ViewContainerRef, ɵrenderComponent as renderComponent, Inject, Injector, ComponentFactoryResolver } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'shell';
 }
-

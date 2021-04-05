35535ff97c6b28e29eef02efc98265f04ca5d22d
Owen Mecham <owen.mecham@adesa.com>
2021-04-04 22:38:06 -0600

    feat: working shared lib

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WalletLibService } from 'wallet-lib';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shell';
+  constructor(private walletService: WalletLibService) {
+    this.walletService.purchase(100);
+  }
 }
---
+++ projects/wallet-lib/src/lib/wallet-lib.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { WalletLibComponent } from './wallet-lib.component';
+
+
+
+@NgModule({
+  declarations: [WalletLibComponent],
+  imports: [
+  ],
+  exports: [WalletLibComponent]
+})
+export class WalletLibModule { }

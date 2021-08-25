75d1b193541e17753990c3ebfaca3ec24e5983b6
Owen Mecham <owen.mecham@adesa.com>
2021-08-25 00:46:09 -0600

    feat: dynamic import

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,2 +1,6 @@
-import('./bootstrap')
-	.catch(err => console.error(err));
+import { loadRemoteEntry } from '@angular-architects/module-federation';
+
+Promise.all([loadRemoteEntry('http://localhost:3000/remoteEntry.js', 'mfe1')])
+  .catch((err) => console.error('Error loading remote entries', err))
+  .then(() => import('./bootstrap'))
+  .catch((err) => console.error(err));

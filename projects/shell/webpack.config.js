54ccf11958c3b3b3ab69bc9ec1655aa0f50d95a9
Owen Mecham <owen.mecham@adesa.com>
2021-11-15 14:12:16 -0700

    feat: dynamic module federation

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
 
       // For hosts (please adjust)
       remotes: {
-        "mfe1": "mfe1@http://localhost:3000/remoteEntry.js",
+        // "mfe1": "mfe1@http://localhost:3000/remoteEntry.js",
 
       },
 

19cc091a7bf3e36babd5d174e62b5700699879d6
Owen Mecham <owen.mecham@adesa.com>
2021-11-15 14:31:26 -0700

    feat: shared library working

--- projects/mfe1/src/app/cryptos/cryptos-search/cryptos-search.component.ts
+++ projects/mfe1/src/app/cryptos/cryptos-search/cryptos-search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WalletLibService } from 'wallet-lib';
 
 @Component({
   selector: 'app-cryptos-search',
@@ -6,13 +7,18 @@ import { Component } from '@angular/core';
 })
 export class CryptosSearchComponent {
   active = false;
+  balance: number;
+  constructor(private walletService: WalletLibService) {
+    this.balance = this.walletService.balance;
+  }
   search(): void {
     this.active = true;
   }
-
   reset(): void {
     this.active = false;
   }
-
-  purchase(): void {}
+  purchase(): void {
+    this.walletService.purchase(100);
+    this.balance = this.walletService.balance;
+  }
 }
--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -6,7 +6,7 @@ const share = mf.share;
 const sharedMappings = new mf.SharedMappings();
 sharedMappings.register(
   path.join(__dirname, '../../tsconfig.json'),
-  [/* mapped paths to share */]);
+  ['wallet-lib']);
 
 module.exports = {
   output: {
